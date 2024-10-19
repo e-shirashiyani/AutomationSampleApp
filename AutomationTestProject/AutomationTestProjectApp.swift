b35911ac4c82b6d9d1502822710a02e338529317
e.shirashiyani <e.shirashiyani>
2024-10-19 19:47:21 +0330

    Initial Commit

---
+++ AutomationTestProject/AutomationTestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutomationTestProjectApp.swift
+//  AutomationTestProject
+//
+//  Created by e.shirashiyani on 10/19/24.
+//
+
+import SwiftUI
+
+@main
+struct AutomationTestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
